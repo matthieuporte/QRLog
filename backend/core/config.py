6ee77e725d1a280d1b39aaa20ec670732ce5c648
vincent porte <vincent@neuralia.co>
2022-11-13 01:39:26 +0100

    adjust DATABASE_URL env var to PAAS provider requirements

--- backend/core/config.py
+++ backend/core/config.py
@@ -12,16 +12,12 @@ class Settings:
     PROJECT_NAME: str = "QRLog"
     PROJECT_VERSION: str = "1.0.0"
 
-    POSTGRES_USER: str = os.getenv("POSTGRES_USER")
-    POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
-    POSTGRES_SERVER: str = os.getenv("POSTGRES_SERVER", "localhost")
-    POSTGRES_PORT: str = os.getenv("POSTGRES_PORT", 5432)
-    POSTGRES_DB: str = os.getenv("POSTGRES_DB", "tdd")
+    # POSTGRES_USER: str = os.getenv("POSTGRES_USER")
+    # POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+    # POSTGRES_SERVER: str = os.getenv("POSTGRES_SERVER", "localhost")
+    # POSTGRES_PORT: str = os.getenv("POSTGRES_PORT", 5432)
+    # POSTGRES_DB: str = os.getenv("POSTGRES_DB", "tdd")
     DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
 
 settings = Settings()
-print("- - - -")
-print(datetime.now().strftime("%H:%M:%S"))
-print(settings.POSTGRES_USER, "on", settings.POSTGRES_SERVER, settings.POSTGRES_PORT)
-print("- - - -")
